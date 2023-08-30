aa45fdd34bcee915aec231ed9a391b79cf3659c6
Alexandre Souza <alexandresouza9@gmail.com>
2023-08-30 20:16:47 -0300

    commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,36 +1,39 @@
+package desafio.dio.conta.bancaria;
 
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar  a classe Scanner
-        Scanner sc = new main(System.in);
-        int numero;
-        String agencia;
-        String nomeCliente;
-        double saldo;
 
-        //Exibir as mensagens para o nosso usuário
-        System.out.println("=============CONTA BANCÁRIA===============");
-        System.out.println();
-        System.out.println("Por favor, digite o número da Conta: (1234)");
-        numero = sc.next();
+	public static void main(String[] args) {
 
-        System.out.println("Digite o número da Angência (123-8)");
-        agencia = sc.next();
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
 
-        System.out.printl("Digite o seu nome: ");
-        nomeCliente = sc.next();
-        sc.nextLine();
+		System.out.println("---------------CONTA BANCÁRIA---------------");
+		System.out.println();
+		System.out.println("Por favor, digite o número da Conta: (1234)");
+		numero = sc.nextInt();
 
-        //Obter pela scanner os valores digitados no terminal
-        System.out.println("O seu saldo atual: ");
-        saldo = sc.nextDouble();
+		System.out.println("Agora digite o número da Agência: (123-8)");
+		agencia = sc.next();
 
-        // Exibir a mensagem conta criada
-        System.out.println();
-        System.out.println("Olá %s, obrigado por escolher o nosso banco para abrir sua conta" + "sua agência é %s, " + "conta %d e seu saldo %.2f já está disponível " + "para saque", nomeCliente, agencia, numero, saldo );
+		System.out.println("Me informe seu nome: ");
+		nomeCliente = sc.next();
+		sc.nextLine();
 
-        sc.close();
-    }
+		System.out.println("Agora me informa seu saldo atual: ");
+		saldo = sc.nextDouble();
+
+		System.out.println();
+		System.out.printf("Olá %s, obrigado por criar uma conta em "
+				+ "nosso banco, sua agência é %s, "
+				+ "conta %d e seu saldo %.2f já está disponível"
+				+ " para saque.", nomeCliente, agencia, numero, saldo);
+
+		sc.close();
+	}
+
 }
